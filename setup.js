ea1c65e260044ef36731b4a2954a2ac813d33532
Emin Khateeb <emin@emin.ch>
2018-03-21 10:40:22 +0100

    now also removing index.js in the setup script

--- setup.js
+++ setup.js
@@ -17,6 +17,7 @@ writeFile('package.json', JSON.stringify(packageJson, null, 2));
 
 execSync(`npm i ${devDependencies.join(' ')} --save-dev --save-exact`);
 
+deleteFile('index.js');
 deleteFile('App.js');
 deleteFile('__tests__/App.js');
 deleteFile('.flowconfig');
