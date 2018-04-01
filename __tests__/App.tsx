126df709d6941c3009903782f107ee28a705ceb7
Steven Cramer <Steven.Cramer@LceCorp.com>
2018-04-01 09:15:57 +0000

    Update import in test
    Update the import in __tests__\App.tsx to align with react-native

--- __tests__/App.tsx
+++ __tests__/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import * as renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
