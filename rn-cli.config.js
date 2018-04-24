80e970bda898d8c12638efb3bd0b370558f13cc6
Emin Khateeb <emin@emin.ch>
2018-04-24 21:14:46 +0200

    added standardjs to lint the setup.js and now using consistently 2 spaces indent

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    getTransformModulePath() {
-        return require.resolve('react-native-typescript-transformer');
-    },
-    getSourceExts() {
-        return ['ts', 'tsx'];
-    }
-}
+  getTransformModulePath() {
+    return require.resolve('react-native-typescript-transformer');
+  },
+  getSourceExts() {
+    return ['ts', 'tsx'];
+  },
+};
--- setup.js
+++ setup.js
@@ -1,56 +1,56 @@
-const fs = require('fs');
-const path = require('path');
-const { execSync } = require('child_process');
+const fs = require('fs')
+const path = require('path')
+const {execSync} = require('child_process')
 
-const packageJson = require('./package.json');
-const jestConfig = require('./jest.json');
-const devDependencies = require('./devDependencies.json');
+const packageJson = require('./package.json')
+const jestConfig = require('./jest.json')
+const devDependencies = require('./devDependencies.json')
 
 const deleteFile = (fileName) => {
-    const filePath = path.join(process.cwd(), fileName);
+  const filePath = path.join(process.cwd(), fileName)
 
-    if (!fs.existsSync(filePath)) {
-        return;
-    }
+  if (!fs.existsSync(filePath)) {
+    return
+  }
 
-    fs.unlinkSync(filePath);
-};
-const writeFile = (fileName, data) => fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  fs.unlinkSync(filePath)
+}
+const writeFile = (fileName, data) => fs.writeFileSync(path.join(process.cwd(), fileName), data)
 const isYarnAvailable = () => {
-    try {
-        execSync('yarn --version', { stdio: 'ignore' });
-        return true;
-    } catch (exc) {
-        return false;
-    }
-};
-
-const packageManager = isYarnAvailable() ? 'yarn' : 'npm';
-const execOptions = { stdio: 'inherit' };
-
-console.log('🔄 Please wait...\n');
-
-packageJson.scripts.start = `${packageJson.scripts.start} --config ../../../../rn-cli.config.js`;
-packageJson.jest = Object.assign(packageJson.jest, jestConfig);
-writeFile('package.json', JSON.stringify(packageJson, null, 2));
-
-console.log(`📦 Installing dependencies with ${packageManager}...\n`);
-
-switch (packageManager) {
-    case 'yarn':
-        execSync(`yarn add ${devDependencies.join(' ')} --dev --exact`, execOptions);
-        break;
-    default:
-        execSync(`npm i ${devDependencies.join(' ')} --save-dev --save-exact`, execOptions);
+  try {
+    execSync('yarn --version', {stdio: 'ignore'})
+    return true
+  } catch (exc) {
+    return false
+  }
 }
 
-deleteFile('App.js');
-deleteFile('__tests__/App.js');
-deleteFile('.flowconfig');
-deleteFile('devDependencies.json');
-deleteFile('jest.json');
-deleteFile('README.md');
-deleteFile('LICENSE');
-deleteFile('setup.js');
+const packageManager = isYarnAvailable() ? 'yarn' : 'npm'
+const execOptions = {stdio: 'inherit'}
 
-console.log(`\n✅ Setup completed! You can now start with: ${packageManager} start\n`);
+console.log('🔄 Please wait...\n')
+
+packageJson.scripts.start = `${packageJson.scripts.start} --config ../../../../rn-cli.config.js`
+packageJson.jest = Object.assign(packageJson.jest, jestConfig)
+writeFile('package.json', JSON.stringify(packageJson, null, 2))
+
+console.log(`📦 Installing dependencies with ${packageManager}...\n`)
+
+switch (packageManager) {
+  case 'yarn':
+    execSync(`yarn add ${devDependencies.join(' ')} --dev --exact`, execOptions)
+    break
+  default:
+    execSync(`npm i ${devDependencies.join(' ')} --save-dev --save-exact`, execOptions)
+}
+
+deleteFile('App.js')
+deleteFile('__tests__/App.js')
+deleteFile('.flowconfig')
+deleteFile('devDependencies.json')
+deleteFile('jest.json')
+deleteFile('README.md')
+deleteFile('LICENSE')
+deleteFile('setup.js')
+
+console.log(`\n✅ Setup completed! You can now start with: ${packageManager} start\n`)
