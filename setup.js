c66f735c19a7ff65c81d109729e92bf0e731c5a8
Emin Khateeb <emin93@users.noreply.github.com>
2018-06-14 10:05:17 +0200

    removed deletes of non-existing files

--- setup.js
+++ setup.js
@@ -4,15 +4,7 @@ const path = require('path')
 const packageJson = require('./package.json')
 const jestConfig = require('./jest.json')
 
-const deleteFile = (fileName) => {
-  const filePath = path.join(process.cwd(), fileName)
-
-  if (!fs.existsSync(filePath)) {
-    return
-  }
-
-  fs.unlinkSync(filePath)
-}
+const deleteFile = (fileName) => fs.unlinkSync(filePath)
 const writeFile = (fileName, data) => fs.writeFileSync(path.join(process.cwd(), fileName), data)
 
 console.log('🔄 Setting up...')
@@ -21,9 +13,7 @@ packageJson.jest = Object.assign(packageJson.jest, jestConfig)
 writeFile('package.json', JSON.stringify(packageJson, null, 2))
 
 deleteFile('App.js')
-deleteFile('__tests__/App.js')
 deleteFile('.flowconfig')
-deleteFile('devDependencies.json')
 deleteFile('jest.json')
 deleteFile('README.md')
 deleteFile('LICENSE')
