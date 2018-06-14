caae886d8f7eecfc88a4c14a5764e425af2f87c1
Emin Khateeb <emin@emin.ch>
2018-06-14 12:25:57 +0200

    setup script is now working dir independent

--- setup.js
+++ setup.js
@@ -4,20 +4,20 @@ const path = require('path')
 const packageJson = require('./package.json')
 const jestConfig = require('./jest.json')
 
-const deleteFile = (fileName) => fs.unlinkSync(path.join(process.cwd(), fileName))
-const writeFile = (fileName, data) => fs.writeFileSync(path.join(process.cwd(), fileName), data)
+const deleteFile = (fileName) => fs.unlinkSync(path.join(__dirname, fileName))
+const writeFile = (fileName, data) => fs.writeFileSync(path.join(__dirname, fileName), data)
 
 console.log('🔄 Setting up...')
 
 packageJson.jest = Object.assign(packageJson.jest, jestConfig)
 writeFile('package.json', JSON.stringify(packageJson, null, 2))
 
+deleteFile('.flowconfig')
 deleteFile('App.js')
 deleteFile('devDependencies.json')
-deleteFile('.flowconfig')
 deleteFile('jest.json')
-deleteFile('README.md')
 deleteFile('LICENSE')
+deleteFile('README.md')
 deleteFile('setup.js')
 
 console.log(`✅ Setup completed!`)
