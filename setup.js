deb1eadafbac3d707360576635fcafaf52887411
Steve Lamb <steve@getscribeware.com>
2019-03-12 13:48:14 -0700

    fixed usage with yarn

--- setup.js
+++ setup.js
@@ -10,8 +10,13 @@ const projectFilesToDelete = ['.flowconfig', 'App.js', '__tests__/App-test.js']
 const templateFilesToDelete = ['setup.js', 'README.md', 'LICENSE']
 
 const projectPath = path.join(__dirname, '..', '..')
-const deleteProjectFile = fileName => fs.unlinkSync(path.join(projectPath, fileName))
-const deleteTemplateFile = fileName => fs.unlinkSync(path.join(__dirname, fileName))
+
+const deleteIfExists = filePath => {
+  if (fs.existsSync(filePath)) fs.unlinkSync(filePath);
+}
+
+const deleteProjectFile = fileName => deleteIfExists(path.join(projectPath, fileName))
+const deleteTemplateFile = fileName => deleteIfExists(path.join(__dirname, fileName))
 
 projectFilesToDelete.forEach(deleteProjectFile)
 templateFilesToDelete.forEach(deleteTemplateFile)
