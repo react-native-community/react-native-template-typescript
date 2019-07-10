bf45a7bfd1e8f548d61c8791546711462054ad99
Emin Khateeb <emin@emin.ch>
2019-07-10 07:31:51 +0200

    updated template to match the latest RN template

--- template/ios/HelloWorldTests/HelloWorldTests.m
+++ template/ios/HelloWorldTests/HelloWorldTests.m
@@ -12,7 +12,7 @@
 #import <React/RCTRootView.h>
 
 #define TIMEOUT_SECONDS 600
-#define TEXT_TO_LOOK_FOR @"Welcome to React"
+#define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
 @interface HelloWorldTests : XCTestCase
 
