eacd45e3dda77ae2e1d4c333dd3f536bda631afe
werder <manuel.werder@gmail.com>
2021-08-13 22:43:00 +0200

    correcting template text on the welcome screen (#214)
    Co-authored-by: Manuel werder <manuel@docomondo.com>

--- template/App.tsx
+++ template/App.tsx
@@ -74,7 +74,7 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
