55f9da4bb953eba778594668a13c79563f190000
Frieder Bluemle <frieder.bluemle@gmail.com>
2021-12-16 10:58:14 +0100

    Fix ESLint setup

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
   },
 };
