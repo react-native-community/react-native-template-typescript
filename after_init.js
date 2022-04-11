b43ab0611d9761e485545ba2b0122d283dcbbb09
Danny <dannyhyunsoowilliams@gmail.com>
2022-04-11 20:10:03 +0100

    use fs instead of shelljs

--- after_init.js
+++ after_init.js
@@ -1,4 +1,26 @@
 #!/usr/bin/env node
-var shell = require("shelljs");
-shell.mv("_ondevice", ".ondevice");
-shell.mv("_storybook", ".storybook");
+
+const fs = require("fs");
+const path = require("path");
+
+const ondeviceOldFolder = path.resolve(__dirname, "_ondevice");
+const ondeviceNewFolder = path.resolve(__dirname, ".ondevice");
+
+fs.rename(ondeviceOldFolder, ondeviceNewFolder, function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Successfully moved the ondevice folder!");
+  }
+});
+
+const storybookCurrentPath = path.resolve(__dirname, "_storybook");
+const storybookNewfolder = path.resolve(__dirname, ".storybook");
+
+fs.rename(storybookCurrentPath, storybookNewfolder, function (err) {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Successfully moved the storybook folder!");
+  }
+});
