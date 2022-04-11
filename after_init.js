235de03fa336c81106b26611d76b54c93e929fe1
Danny <dannyhyunsoowilliams@gmail.com>
2022-04-11 20:16:35 +0100

    fix: script path

--- after_init.js
+++ after_init.js
@@ -1,26 +1,19 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
-const path = require("path");
 
-const ondeviceOldFolder = path.resolve(__dirname, "_ondevice");
-const ondeviceNewFolder = path.resolve(__dirname, ".ondevice");
-
-fs.rename(ondeviceOldFolder, ondeviceNewFolder, function (err) {
+fs.rename("_ondevice", ".ondevice", function (err) {
   if (err) {
     throw err;
   } else {
-    console.log("Successfully moved the ondevice folder!");
+    console.log("Successfully configured the ondevice folder!");
   }
 });
 
-const storybookCurrentPath = path.resolve(__dirname, "_storybook");
-const storybookNewfolder = path.resolve(__dirname, ".storybook");
-
-fs.rename(storybookCurrentPath, storybookNewfolder, function (err) {
+fs.rename("_storybook", ".storybook", function (err) {
   if (err) {
     throw err;
   } else {
-    console.log("Successfully moved the storybook folder!");
+    console.log("Successfully configured the storybook folder!");
   }
 });
