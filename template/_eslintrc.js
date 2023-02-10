336e7e9ea3ac6bb5674e27cb88c28600c139b5db
YutaSugimura <nextc3m@gmail.com>
2023-02-10 17:55:35 +0900

    fix: Adjust templates and differences in react-native repository

--- template/_eslintrc.js
+++ template/_eslintrc.js
@@ -1,14 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
 };
